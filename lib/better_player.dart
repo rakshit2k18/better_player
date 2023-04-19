088ae4158c4f3afcce81b90be17e1494830a953c
rakshit2k18 <rakshit.mathpal@phando.com>
2023-04-19 16:11:18 +0530

    updated for ios

--- lib/better_player.dart
+++ lib/better_player.dart
@@ -1,65 +1,6 @@
-// import 'package:flutter/material.dart';
-// import 'package:better_player/better_player.dart';
-
-// class VideoScreen extends StatefulWidget {
-//   final String videoUrl;
-
-//   VideoScreen({required this.videoUrl});
-
-//   @override
-//   _VideoScreenState createState() => _VideoScreenState();
-// }
-
-// class _VideoScreenState extends State<VideoScreen> {
-//   BetterPlayerController? _betterPlayerController;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _setupVideoPlayer();
-//   }
-
-//   void _setupVideoPlayer() {
-//     _betterPlayerController = BetterPlayerController(
-//       const  BetterPlayerConfiguration(
-//             aspectRatio: 16 / 9,
-//             autoPlay: true,
-//             looping: true,
-//             autoDetectFullscreenAspectRatio: true,
-//             controlsConfiguration: BetterPlayerControlsConfiguration(
-//                enablePlaybackSpeed: true,
-//                enableProgressBarDrag: true,
-//                showControls: true,
-
-//                 enableSkips: false,
-//                 enableOverflowMenu: false,
-//                 enableFullscreen: true)),
-//         betterPlayerDataSource:
-//             BetterPlayerDataSource(BetterPlayerDataSourceType.network, widget.videoUrl));
-//   }
-
-//   @override
-//   void dispose() {
-//     _betterPlayerController!.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text('Video Screen')),
-//       body: Container(
-//         child: AspectRatio(
-//           aspectRatio: 16 / 9,
-//           child: BetterPlayer(controller: _betterPlayerController!),
-//         ),
-//       ),
-
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:better_player/better_player.dart';
+import 'package:flutter/services.dart';
 
 class VideoScreen extends StatefulWidget {
   final String videoUrl;
@@ -81,21 +22,31 @@ class _VideoScreenState extends State<VideoScreen> {
 
   void _setupVideoPlayer() {
     _betterPlayerController = BetterPlayerController(
-        const BetterPlayerConfiguration(
-            aspectRatio: 16 / 9,
-            autoPlay: true,
-            looping: true,
-            autoDetectFullscreenAspectRatio: true,
-            controlsConfiguration: BetterPlayerControlsConfiguration(
-                enablePlaybackSpeed: true,
-                enableProgressBarDrag: true,
-                showControls: true,
-                enableSkips: true,
-                enableOverflowMenu: true,
+         BetterPlayerConfiguration(
+          aspectRatio: 16 / 9,
+          autoPlay: true,
+          looping: true,
+          autoDetectFullscreenAspectRatio: true,
+          controlsConfiguration: BetterPlayerControlsConfiguration(
+              enablePlaybackSpeed: true,
+              enableProgressBarDrag: true,
+              showControls: true,
+              enableSkips: true,
+              enableOverflowMenu: true,
+            
+              enableFullscreen: true),
+  deviceOrientationsAfterFullScreen: [
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+        DeviceOrientation.landscapeLeft,
+        DeviceOrientation.landscapeRight,
+      ],
+              
 
-                enableFullscreen: true)),
-        betterPlayerDataSource:
-            BetterPlayerDataSource(BetterPlayerDataSourceType.network, widget.videoUrl));
+
+        ),
+        betterPlayerDataSource: BetterPlayerDataSource(
+            BetterPlayerDataSourceType.network, widget.videoUrl));
   }
 
   @override
@@ -117,4 +68,3 @@ class _VideoScreenState extends State<VideoScreen> {
     );
   }
 }
-
